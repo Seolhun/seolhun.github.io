fa59cba60898a2c819da74b0077654ac1f75ec44
Seolhun <shun10114@gmail.com>
2019-09-13 04:08:16 +0900

    Add starts canvas but not used now

--- src/containers/canvases/hooks/useCanvas.tsx
+++ src/containers/canvases/hooks/useCanvas.tsx
@@ -1,5 +1,6 @@
-import { LocalizeThemes } from '@seolhun/localize-components-styled-types';
 import { useEffect, useRef } from 'react';
+
+import { LocalizeThemes } from '@seolhun/localize-components-styled-types';
 
 type CanvasRef = HTMLCanvasElement | null;
 
@@ -14,17 +15,22 @@ const useDrawCanvas = (canvasRef: CanvasRef) => {
   const ctx = canvas ? canvas.getContext('2d') : null;
   const rate = 60;
   const arc = 100;
-  let time: number = 0;
   const size = 7;
   const speed = 20;
   const parts: any[] = [];
   const colors = Object.values(LocalizeThemes);
   const mouse: any = { x: 0, y: 0 };
+  let time: number = 0;
 
   canvas.setAttribute('width', `${width}`);
   canvas.setAttribute('height', `${height}`);
 
-  const create = () => {
+  const MouseMove = (e: MouseEvent) => {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY - document.documentElement.scrollTop;
+  };
+
+  const createCircles = () => {
     for (let i = 0; i < arc; i++) {
       parts[i] = {
         x: Math.ceil(Math.random() * width),
@@ -37,20 +43,64 @@ const useDrawCanvas = (canvasRef: CanvasRef) => {
     }
   };
 
+  const useDrawStar = (
+    cx: number,
+    cy: number,
+    spikes: number,
+    outerRadius: number,
+    innerRadius: number,
+  ) => {
+    if (!ctx) {
+      return;
+    }
+
+    let rot = (Math.PI / 2) * 3;
+    let x = cx;
+    let y = cy;
+    const step = Math.PI / spikes;
+
+    ctx.strokeStyle = '#000';
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - outerRadius);
+    for (let i = 0; i < spikes; i++) {
+      x = cx + Math.cos(rot) * outerRadius;
+      y = cy + Math.sin(rot) * outerRadius;
+      ctx.lineTo(x, y);
+      rot += step;
+
+      x = cx + Math.cos(rot) * innerRadius;
+      y = cy + Math.sin(rot) * innerRadius;
+      ctx.lineTo(x, y);
+      rot += step;
+    }
+    ctx.lineTo(cx, cy - outerRadius);
+    ctx.closePath();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'blue';
+    ctx.stroke();
+    ctx.fillStyle = 'skyblue';
+    ctx.fill();
+  };
+
   const particles = () => {
     if (!ctx) {
       return;
     }
-    ctx.clearRect(0, 0, width, height);
     canvas.addEventListener('mousemove', MouseMove, false);
+
+    ctx.clearRect(0, 0, width, height);
     for (let i = 0; i < arc; i++) {
       const li = parts[i];
       let distanceFactor = DistanceBetween(mouse, parts[i]);
+      // useDrawStar(75, 100, 3, 30, 15);
+      // useDrawStar(75, 200, 5, 30, 15);
+
       distanceFactor = Math.max(Math.min(15 - distanceFactor / 10, 10), 1);
       ctx.beginPath();
       ctx.arc(li.x, li.y, li.size * distanceFactor, 0, Math.PI * 2, false);
       ctx.fillStyle = li.c;
       ctx.strokeStyle = li.c;
+
       if (i % 2 === 0) {
         ctx.stroke();
       } else {
@@ -79,18 +129,13 @@ const useDrawCanvas = (canvasRef: CanvasRef) => {
     setTimeout(particles, 1000 / rate);
   };
 
-  const MouseMove = (e: MouseEvent) => {
-    mouse.x = e.pageX;
-    mouse.y = e.pageY - document.documentElement.scrollTop;
-  };
-
   const DistanceBetween = (p1: any, p2: any) => {
     const dx = p2.x - p1.x;
     const dy = p2.y - p1.y;
     return Math.sqrt(dx * dx + dy * dy);
   };
 
-  create();
+  createCircles();
   particles();
 };
 
